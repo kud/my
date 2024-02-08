e0bfae8a64b889d8db9846ec87a39c528c1af9e9
Erwann Mest <m@kud.io>
2024-02-08 12:04:35 +0000

    refactor(profiles/work): Update Bartender version in _globalprotect-connect script

--- profiles/work/bin/_/_globalprotect-connect/index.js
+++ profiles/work/bin/_/_globalprotect-connect/index.js
@@ -38,7 +38,7 @@ const barTender = processes.byName("Bartender")
 const globalProtect = processes.byName("GlobalProtect")
 
 // Closing Bartender as it makes more difficult the situation
-const bartender = Application("Bartender 4")
+const bartender = Application("Bartender 5")
 bartender.quit()
 
 // Opening GlobalProtect
