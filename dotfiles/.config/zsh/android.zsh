28596865229ec6e6f7a2ab773ff43dd7929d305e
Erwann Mest <m@kud.io>
2025-07-06 00:30:56 +0100

    feat(zsh): add environment configuration files for Android, Babel, Homebrew, and OpenSSL

---
+++ dotfiles/.config/zsh/android.zsh
@@ -0,0 +1,10 @@
+################################################################################
+#                                                                              #
+#   🤖 ANDROID ENVIRONMENT                                                     #
+#   ----------------------                                                     #
+#   Android SDK and related variables.                                         #
+#                                                                              #
+################################################################################
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
