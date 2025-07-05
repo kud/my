38670719d0cec07d3f1ae26b8c5d59e058ff18e3
Erwann Mest <m@kud.io>
2025-07-05 23:28:32 +0100

    feat(zsh): add custom functions for tab title, folder creation, and yazi navigation

---
+++ dotfiles/.config/zsh/functions.zsh
@@ -0,0 +1,36 @@
+
+############################################################
+# 🏷️  Set the tab title to current dir
+############################################################
+precmd() {
+  local title=""
+
+  if [[ "$PWD" == "$HOME" ]]; then
+    title="~"
+  else
+    title="${PWD##*/}"
+  fi
+
+  echo -ne "\e]1;$title\a"
+}
+
+
+############################################################
+# 📁 Create a folder and go in it
+############################################################
+function mcd() {
+  mkdir -p "$1" && cd "$1";
+}
+
+
+############################################################
+# 🗂️  Yazi: open and jump to selected dir
+############################################################
+function yy() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
+}
