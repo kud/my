0c439dc85a2f7d9ab2f8ec998f660404acc479c7
Erwann Mest <m@kud.io>
2025-07-06 02:16:37 +0100

    feat(zsh): add autosuggestions configuration and update zshrc to source it

---
+++ dotfiles/.config/zsh/autosuggestions.zsh
@@ -0,0 +1,10 @@
+################################################################################
+#                                                                              #
+#   🤖 ZSH AUTOSUGGESTIONS CONFIGURATION                                       #
+#   -------------------------------------                                      #
+#   Performance and behaviour tweaks for zsh-autosuggestions.                  #
+#                                                                              #
+################################################################################
+
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE="20"
+ZSH_AUTOSUGGEST_USE_ASYNC=1
