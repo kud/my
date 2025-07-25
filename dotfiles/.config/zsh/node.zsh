deeefc1b127277b6fce8efd0774cd3471362954e
Erwann Mest <m@kud.io>
2025-07-25 21:25:00 +0100

    chore(zsh): remove fnm.zsh and update initialization in node.zsh and zshrc files

--- dotfiles/.config/zsh/node.zsh
+++ dotfiles/.config/zsh/node.zsh
@@ -2,8 +2,14 @@
 #                                                                                #
 #   🟢 NODE.JS ENVIRONMENT INITIALISATION                                        #
 #   -------------------------------                                              #
-#   Sets up Node.js global modules path.                                         #
+#   Sets up Node.js via fnm and configures npm global packages.                  #
 #                                                                                #
 # ################################################################################
 
-export NODE_PATH=${HOMEBREW_PREFIX}/lib/node_modules
+export PATH="$(npm config get prefix)/bin:$PATH"
+
+# Initialize fnm (Fast Node Manager)
+eval "$(fnm env --use-on-cd --shell zsh)"
+
+# Set NODE_PATH to the active Node.js version's modules
+# export NODE_PATH=${HOMEBREW_PREFIX}/lib/node_modules
