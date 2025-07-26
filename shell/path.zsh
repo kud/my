189ce8cedbb447f97cb29f65ec71761808827483
Erwann Mest <m@kud.io>
2025-07-27 00:53:47 +0100

    chore(shell): remove commented out Homebrew path from path.zsh

--- shell/path.zsh
+++ shell/path.zsh
@@ -8,7 +8,6 @@
 
 # Homebrew
 export PATH=${HOMEBREW_PREFIX}/sbin:$PATH
-# export PATH=${HOMEBREW_PREFIX}/Cellar/:$PATH
 export PATH=${HOMEBREW_PREFIX}/opt/curl/bin:$PATH
 export PATH=${HOMEBREW_PREFIX}/opt/ruby/bin:$PATH
 export PATH=${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnubin:$PATH
