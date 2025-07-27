246cf57f26390b888fd14c79f0f881bcb0d91fb7
Erwann Mest <m@kud.io>
2025-07-27 23:34:32 +0100

    refactor(zsh): streamline profiling setup and load Homebrew prefix with fallback option

--- shell/homebrew.zsh
+++ shell/homebrew.zsh
@@ -6,4 +6,7 @@
 #                                                                                #
 # ################################################################################
 
+# Set Homebrew prefix (with fallback for older Intel Macs)
+export HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix 2>/dev/null || echo /usr/local)}"
+
 export HOMEBREW_CASK_OPTS=--appdir=/Applications
