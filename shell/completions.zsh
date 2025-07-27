1cecf4923cd6c865e429bdaee58e1edc850205ed
Erwann Mest <m@kud.io>
2025-07-27 01:15:10 +0100

    chore(shell): remove legacy npm completion code from completions.zsh

--- shell/completions.zsh
+++ shell/completions.zsh
@@ -7,49 +7,6 @@
 #     npm completion > ${HOMEBREW_PREFIX}/etc/bash_completion.d/npm            #
 #                                                                              #
 ################################################################################
-## Legacy npm completion for Bash and old Zsh (kept for reference):
-# if type complete &>/dev/null; then
-#   _npm_completion () {
-#     local words cword
-#     if type _get_comp_words_by_ref &>/dev/null; then
-#       _get_comp_words_by_ref -n = -n @ -n : -w words -i cword
-#     else
-#       cword="$COMP_CWORD"
-#       words=("${COMP_WORDS[@]}")
-#     fi
-#
-#     local si="$IFS"
-#     IFS=$'\n' COMPREPLY=($(COMP_CWORD="$cword" \
-#                            COMP_LINE="$COMP_LINE" \
-#                            COMP_POINT="$COMP_POINT" \
-#                            npm completion -- "${words[@]}" \
-#                            2>/dev/null)) || return $?
-#     IFS="$si"
-#     if type __ltrim_colon_completions &>/dev/null; then
-#       __ltrim_colon_completions "${words[cword]}"
-#     fi
-#   }
-#   complete -o default -F _npm_completion npm
-# fi
-# if type compctl &>/dev/null; then
-#   _npm_completion () {
-#     local cword line point words si
-#     read -Ac words
-#     read -cn cword
-#     let cword-=1
-#     read -l line
-#     read -ln point
-#     si="$IFS"
-#     IFS=$'\n' reply=($(COMP_CWORD="$cword" \
-#                        COMP_LINE="$line" \
-#                        COMP_POINT="$point" \
-#                        npm completion -- "${words[@]}" \
-#                        2>/dev/null)) || return $?
-#     IFS="$si"
-#   }
-#   compctl -K _npm_completion npm
-# fi
-
 # Modern Zsh-native npm completion:
 if type compdef &>/dev/null; then
   _npm_completion() {
