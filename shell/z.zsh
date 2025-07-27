56e754f44ac0a9f5666f24ff9b33ad6ac2ce7b8d
Erwann Mest <m@kud.io>
2025-07-27 23:00:47 +0100

    refactor(dotfiles): remove autojump and integrate zoxide into zsh configuration

--- shell/z.zsh
+++
@@ -1,9 +0,0 @@
-# ################################################################################
-#                                                                                #
-#   📁 Z DIRECTORY JUMPER INITIALISATION                                         #
-#   -------------------------------                                              #
-#   Loads z for directory jumping.                                               #
-#                                                                                #
-# ################################################################################
-
-[ -f ${HOMEBREW_PREFIX}/etc/profile.d/z.sh ] && . ${HOMEBREW_PREFIX}/etc/profile.d/z.sh
