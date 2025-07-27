4e10c04660e85633e45aa8a8601406a24852cce5
Erwann Mest <m@kud.io>
2025-07-27 13:47:15 +0100

    feat(directory): add directory navigation options and aliases for enhanced usability

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -6,6 +6,19 @@ alias ....='cd ../../..'
 alias ...='cd ../..'
 alias ..='cd ..'
 
+alias d='dirs -v'          # Print contents of the directory stack
+alias -- -='cd -'          # Go to previous directory
+
+alias 1='cd +1'
+alias 2='cd +2'
+alias 3='cd +3'
+alias 4='cd +4'
+alias 5='cd +5'
+alias 6='cd +6'
+alias 7='cd +7'
+alias 8='cd +8'
+alias 9='cd +9'
+
 
 ############################################################
 # ⚡ Super User
