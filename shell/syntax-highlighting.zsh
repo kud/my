c2f4448673d90934659c3b81cf3e3ae19027a93f
Erwann Mest <m@kud.io>
2025-07-29 23:23:26 +0100

    feat(syntax-highlighting): integrate zsh-syntax-highlighting plugin with new configuration file

---
+++ shell/syntax-highlighting.zsh
@@ -0,0 +1,22 @@
+################################################################################
+#                                                                              #
+#   🎨 ZSH SYNTAX HIGHLIGHTING CONFIGURATION                                  #
+#   ------------------------------------------                                 #
+#   Configures zsh-syntax-highlighting plugin styling and patterns.            #
+#   https://github.com/zsh-users/zsh-syntax-highlighting                       #
+#                                                                              #
+################################################################################
+
+# Only configure if zsh-syntax-highlighting is loaded
+if [[ -n "${ZSH_HIGHLIGHT_VERSION:-}" ]]; then
+  
+  # Set syntax highlighters (equivalent to prezto config)
+  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern line root)
+  
+  # Set syntax highlighting styles
+  ZSH_HIGHLIGHT_STYLES[path]='fg=white'
+  
+  # Set syntax pattern styles  
+  ZSH_HIGHLIGHT_PATTERNS=('rm*-rf*' 'fg=white,bold,bg=red')
+  
+fi
