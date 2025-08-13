5533ba53477f2c17f6692a440479f2664169c8eb
Erwann Mest <m@kud.io>
2025-08-13 22:49:38 +0100

    ♻️ (shell): clean up python.zsh and syntax-highlighting.zsh by removing unnecessary comments and improving readability

--- shell/syntax-highlighting.zsh
+++ shell/syntax-highlighting.zsh
@@ -7,16 +7,8 @@
 #                                                                              #
 ################################################################################
 
-# Only configure if zsh-syntax-highlighting is loaded
 if [[ -n "${ZSH_HIGHLIGHT_VERSION:-}" ]]; then
-  
-  # Set syntax highlighters
   ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern line root)
-  
-  # Set syntax highlighting styles
   ZSH_HIGHLIGHT_STYLES[path]='fg=white'
-  
-  # Set syntax pattern styles  
   ZSH_HIGHLIGHT_PATTERNS=('rm*-rf*' 'fg=white,bold,bg=red')
-  
 fi
