b214bced2174155a1632bff872db32573e322ff2
Erwann Mest <m@kud.io>
2025-08-13 22:15:08 +0100

    📝 (antidote.zsh): update comments to clarify plugin management process and improve readability

--- shell/antidote.zsh
+++ shell/antidote.zsh
@@ -7,25 +7,16 @@
 #                                                                                #
 # ################################################################################
 
-# Ultra high performance Antidote plugin management
-# -----------------------------------------------
-# Uses static plugin files in $ZDOTDIR or $HOME for maximum speed and portability.
-
-# Set plugin file paths (using shell/plugins.txt as the source)
 zsh_plugins_txt="$MY/shell/plugins.txt"
 zsh_plugins_zsh="$HOME/.zsh_plugins.zsh"
 
-# Ensure the plugins.txt file exists
 [[ -f "$zsh_plugins_txt" ]] || touch "$zsh_plugins_txt"
 
-# Lazy-load Antidote from its functions directory (fastest method)
 fpath=( $HOMEBREW_PREFIX/share/antidote/functions $fpath )
 autoload -Uz antidote
 
-# Generate a new static file whenever plugins.txt is updated
 if [[ ! -f "$zsh_plugins_zsh" || "$zsh_plugins_txt" -nt "$zsh_plugins_zsh" ]]; then
   antidote bundle <"$zsh_plugins_txt" >! "$zsh_plugins_zsh"
 fi
 
-# Source the static plugins file
 source "$zsh_plugins_zsh"
