d0b1f31694d302fa72533cea13a2e95fe806a02a
Erwann Mest <m@kud.io>
2025-08-13 22:15:20 +0100

    ♻️ (autosuggestions.zsh): remove commented-out performance and appearance settings for cleaner code

--- shell/autosuggestions.zsh
+++ shell/autosuggestions.zsh
@@ -6,10 +6,6 @@
 #                                                                              #
 ################################################################################
 
-# Performance settings
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE="50"
 ZSH_AUTOSUGGEST_USE_ASYNC=1
-
-# Appearance settings
-# Set highlight style - gray text for suggestions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
