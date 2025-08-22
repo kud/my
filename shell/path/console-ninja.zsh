37e3907c5d381de0fe140bb2a2274547995e91ba
Erwann Mest <m@kud.io>
2025-08-22 15:00:52 +0100

    ✨ (dotfiles): add new path configuration files for various tools and utilities 📝 (dotfiles): update .zshrc to source new path configuration files
    New path configuration files are created for Android, Console Ninja, Homebrew, Node.js, and personal utilities. The .zshrc file is updated to source these new files, allowing for better organization and management of environment variables related to different development tools. This modular approach enhances maintainability and clarity in the shell configuration.

---
+++ shell/path/console-ninja.zsh
@@ -0,0 +1,10 @@
+################################################################################
+#                                                                              #
+#   🥷 CONSOLE NINJA PATH SETUP                                               #
+#   -------------------------                                                 #
+#   Console Ninja development tool PATH modifications.                         #
+#                                                                              #
+################################################################################
+
+# Console Ninja
+export PATH=$HOME/.console-ninja/.bin:$PATH
