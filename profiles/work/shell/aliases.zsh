fbc4c64b04a600e48a8c13e90e1dc46428654360
Erwann Mest <m@kud.io>
2025-12-10 17:52:14 +0000

    ✨ feat(profiles-work): add awsume tool and alias for AWS profile management
    Introduce the awsume package to the work profile's uv package list and add a corresponding alias in the work profile shell aliases for easier AWS profile management. This enhances developer workflow by streamlining AWS credential switching.
    
    - Added awsume to profiles/work/config/packages/uv.yml
    - Created an alias for awsume in profiles/work/shell/aliases.zsh
    - Included reference to awsume documentation for user guidance

---
+++ profiles/work/shell/aliases.zsh
@@ -0,0 +1,11 @@
+#! /usr/bin/env zsh
+
+################################################################################
+#                                                                              #
+#   Work Profile Aliases                                                       #
+#                                                                              #
+################################################################################
+
+# awsume alias for AWS profile management
+# https://awsu.me/general/quickstart.html
+alias awsume=". awsume"
