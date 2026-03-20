20b060200d27713d32ab9624af9c2398705adc0a
Erwann Mest <m@kud.io>
2026-03-20 19:06:09 +0000

    🧹 chore(bin): streamline trackpad connect script logic
    Refactors the `bin/main/trackpad-connect` script to remove unused address-detection function, simplify connection check, and unify 'unpair' under a renamed 'forget' action using a static TRACKPAD_ID. Enhances output clarity and reduces unnecessary code complexity. Also exports ITERM2_SQUELCH_MARK in `shell/iterm.zsh` for improved iTerm2 integration.

--- shell/iterm.zsh
+++ shell/iterm.zsh
@@ -12,6 +12,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+export ITERM2_SQUELCH_MARK=1
+
 if [[ -o interactive ]]; then
   if [ "${ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX-}""$TERM" != "tmux-256color" -a "${ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX-}""$TERM" != "screen" -a "${ITERM_SHELL_INTEGRATION_INSTALLED-}" = "" -a "$TERM" != linux -a "$TERM" != dumb ]; then
     ITERM_SHELL_INTEGRATION_INSTALLED=Yes
